a651698f0eaeeced1538f92779c341d02ce42655
Ganesham Pareek <104836088+Ganesham09@users.noreply.github.com>
2025-01-12 02:01:34 +0530

    enhance sidebar component with active link highlighting and add products page

--- app/admin/components/sidebar.jsx
+++ app/admin/components/sidebar.jsx
@@ -11,6 +11,7 @@ import {
   LibraryBig,
 } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
   const menuList = [
@@ -57,22 +58,32 @@ export default function Sidebar() {
   ];
 
   return (
-    <section className="flex flex-col gap-7 bg-white border-r px-5 py-3 h-screen overflow-hidden md:w-[300px]">
+    <section className="flex flex-col gap-10 bg-white border-r px-5 py-3 h-screen overflow-hidden md:w-[300px]">
       <div className="flex justify-center items-center w-full">
         <img className="h-9 w-auto object-contain" src="/logo.png" alt="Logo" />
       </div>
-      <ul className="flex-1 flex flex-col gap-5">
-        {menuList?.map((item) => {
-          return (
-            <Link href={item?.link}>
-              <li className="flex items-center gap-3 px-4 py-2 rounded-xl cursor-pointer hover:bg-gray-100 font-semibold">
-                {item?.icon}
-                {item?.name}
-              </li>
-            </Link>
-          );
+      <ul className="flex-1 flex flex-col gap-4">
+        {menuList?.map((item, key) => {
+          return <Tab item={item} key={key} />;
         })}
       </ul>
     </section>
+  );
+}
+
+function Tab({ item }) {
+  const pathname = usePathname();
+  const isSelected = pathname === item?.link;
+  return (
+    <Link href={item?.link}>
+      <li
+        className={`flex items-center gap-3 px-4 py-2 rounded-xl  font-semibold ease-soft-spring transition-all duration-300 ${
+          isSelected ? "bg-red-400 text-white" : "bg-white text-black"
+        }`}
+      >
+        {item?.icon}
+        {item?.name}
+      </li>
+    </Link>
   );
 }
---
+++ app/admin/products/page.jsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold text-center my-4">Products</h1>
+    </main>
+  );
+}
