119591cceadc52469dc612882c27e425d58139bd
Ganesham Pareek <104836088+Ganesham09@users.noreply.github.com>
2025-01-12 02:19:33 +0530

    sidebar-70% complete

--- app/admin/components/sidebar.jsx
+++ app/admin/components/sidebar.jsx
@@ -9,6 +9,7 @@ import {
   UserRound,
   Star,
   LibraryBig,
+  LogOut,
 } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -59,14 +60,20 @@ export default function Sidebar() {
 
   return (
     <section className="flex flex-col gap-10 bg-white border-r px-5 py-3 h-screen overflow-hidden md:w-[300px]">
-      <div className="flex justify-center items-center w-full">
+      <div className="flex justify-center items-center w-full py-4">
         <img className="h-9 w-auto object-contain" src="/logo.png" alt="Logo" />
       </div>
-      <ul className="flex-1 flex flex-col gap-4">
+      <ul className="flex-1 h-full overflow-y-auto flex flex-col gap-4">
         {menuList?.map((item, key) => {
           return <Tab item={item} key={key} />;
         })}
       </ul>
+      <div className="flex justify-center items-center ">
+        <button className="flex gap-2 items-center px-3 py-2 hover:bg-red-400  rounded-xl font-semibold w-full justify-center ease-soft-spring transition-all duration-400 ">
+          <LogOut className="h-5 w-5" />
+          Logout
+        </button>
+      </div>
     </section>
   );
 }
